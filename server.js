7cd5be388702c55c31d45384ac91e37d07e98a01
tanishlalia03-ai <tanishlalia03@gmail.com>
2025-11-07 10:34:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
 });
 
 app.listen(PORT, () => {
